1e91bbd84aad81ede295c4175d13b4a4593e2d60
ivah9 <ivelina.hadzhieva@yahoo.com>
2025-01-21 22:27:41 +0200

    install and set up express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req,res)=> {
+    res.send('It works');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
